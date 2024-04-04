8c254a73af3a737165f07795e46faaa581748851
Rahul Balhotra <40921516+rahulbalhotra@users.noreply.github.com>
2024-04-04 18:26:16 +0530

    transcriber.py

---
+++ transcriber.py
@@ -0,0 +1,13 @@
+from tempfile import NamedTemporaryFile
+import streamlit as st
+import whisper
+
+audio = st.file_uploader("Upload an audio file", type=["mp3"])
+
+if audio is not None:
+    with NamedTemporaryFile(suffix="mp3") as temp:
+        temp.write(audio.getvalue())
+        temp.seek(0)
+        model = whisper.load_model("base")
+        result = model.transcribe(temp.name)
+        st.write(result["text"])
